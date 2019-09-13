fbdd6109816e07a2f2f75ebaaf89796e29e8bb6c
Dmitry Fedyuk <dfediuk@gmail.com>
2019-09-13 16:48:36 +0600

    no message

--- Setup/InstallSchema.php
+++ Setup/InstallSchema.php
@@ -9,62 +9,33 @@ class InstallSchema implements InstallSchemaInterface {
 	/**
 	 * {@inheritdoc}
 	 */
-	function install(SchemaSetupInterface $setup, ModuleContextInterface $context)
-	{
+	function install(SchemaSetupInterface $setup, ModuleContextInterface $context) {
 		$connection = $setup->getConnection();
 		$setup->startSetup();
-		$table = $connection->newTable($setup->getTable(ExportResource::TABLE))
-			->addColumn(
-				'entity_id',
-				\Magento\Framework\DB\Ddl\Table::TYPE_INTEGER,
-				null,
-				['identity' => true, 'unsigned' => true, 'nullable' => false, 'primary' => true],
-				'Config Id'
-			)->addColumn(
-				'order_id',
-				\Magento\Framework\DB\Ddl\Table::TYPE_INTEGER,
-				null,
-				['unsigned' => true, 'nullable' => false,],
-				'Order Reference'
-			)->addColumn(
-				'queued_at',
-				\Magento\Framework\DB\Ddl\Table::TYPE_TIMESTAMP,
-				null,
-				['nullable' => false, 'default' => \Magento\Framework\DB\Ddl\Table::TIMESTAMP_INIT],
-				'Queued At'
-			)->addColumn(
-				'synced_at',
-				\Magento\Framework\DB\Ddl\Table::TYPE_TIMESTAMP,
-				null,
-				['nullable' => true, 'default' => null],
-				'Synced At'
-			)->addColumn(
-				'synced',
-				\Magento\Framework\DB\Ddl\Table::TYPE_SMALLINT,
-				2,
-				['nullable' => false, 'default' => 0],
-				'Is Synchronized?'
-			)->addColumn(
-				'last_error',
-				\Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
-				'1k',
-				[],
-				'Last Sync Error'
-			)->addForeignKey(
-				$connection->getForeignKeyName(ExportResource::TABLE, 'order_id', 'sales_order', 'entity_id'),
-				'order_id',
-				'sales_order',
-				'entity_id',
-				\Magento\Framework\DB\Ddl\Table::ACTION_CASCADE
-			)->setComment(
-				'Sales Order Export Queue'
-			);
-		$connection->createTable($table);
-
+		$t = $connection->newTable($setup->getTable(ExportResource::TABLE));
+		$t->addColumn(
+			'entity_id', Table::TYPE_INTEGER, null
+			,['identity' => true, 'unsigned' => true, 'nullable' => false, 'primary' => true]
+			,'Config Id'
+		);
+		$t->addColumn('order_id', Table::TYPE_INTEGER, null,['unsigned' => true, 'nullable' => false], 'Order Reference');
+		$t->addColumn('queued_at', Table::TYPE_TIMESTAMP, null,
+			['nullable' => false, 'default' => Table::TIMESTAMP_INIT], 'Queued At'
+		);
+		$t->addColumn('synced_at', Table::TYPE_TIMESTAMP, null, ['nullable' => true, 'default' => null], 'Synced At');
+		$t->addColumn('synced', Table::TYPE_SMALLINT, 2, ['nullable' => false, 'default' => 0], 'Is Synchronized?');
+		$t->addColumn('last_error', Table::TYPE_TEXT, '1k', [], 'Last Sync Error');
+		$t->addForeignKey(
+			$connection->getForeignKeyName(ExportResource::TABLE, 'order_id', 'sales_order', 'entity_id'),
+			'order_id',
+			'sales_order',
+			'entity_id',
+			Table::ACTION_CASCADE
+		)->setComment('Sales Order Export Queue');
+		$connection->createTable($t);
 		$connection->addColumn(
 			$setup->getTable('sales_order_grid'),
-			'export_synced',
-			[
+			'export_synced', [
 				'type' => Table::TYPE_SMALLINT,
 				'length' => 2,
 				'comment' => 'Export Synced',
@@ -72,7 +43,6 @@ class InstallSchema implements InstallSchemaInterface {
 				'nullable' => false
 			]
 		);
-
 		$setup->endSetup();
 	}
 }
