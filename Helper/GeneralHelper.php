0b5f8db17f6e14b153ea11e153e51161e0f68a9c
Dmitry Fedyuk <dfediuk@gmail.com>
2019-09-13 20:25:59 +0600

    no message

--- Helper/GeneralHelper.php
+++ Helper/GeneralHelper.php
@@ -1,9 +1,11 @@
 <?php
 namespace Julio\Order\Helper;
+use DOMDocument as D;
 use Magento\Framework\App\Helper\AbstractHelper;
 use Magento\Framework\App\Helper\Context;
 use Magento\Framework\Encryption\EncryptorInterface;
-use Magento\Sales\Model\Order\Item;
+use Magento\Sales\Model\Order as O;
+use Magento\Sales\Model\Order\Item as OI;
 use Magento\Store\Model\StoreManagerInterface;
 class GeneralHelper extends AbstractHelper {
 	const PATH = 'julio_intelisis/order/%s';
@@ -81,76 +83,69 @@ class GeneralHelper extends AbstractHelper {
 	}
 
 	/**
-	 * Xml representation of order
-	 * @param \Magento\Sales\Model\Order $order
+	 * 2019-09-13
+	 * @param O $o  
+	 * @return D
 	 */
-	function convertToXml(\Magento\Sales\Model\Order $order)
-	{
-		$xml = new \DOMDocument('1.0', 'UTF-8');
-		$xml->formatOutput = true;
-		$xml->preserveWhiteSpace = false;
-		$orderEl = $xml->createElement('order');
+	function convertToXml(O $o) {
+		$d = new D('1.0', 'UTF-8'); /** @var D $d */
+		$d->formatOutput = true;
+		$d->preserveWhiteSpace = false;
+		$orderEl = $d->createElement('order');
 		$orderEl->setAttribute('xmlns:xsd', 'http://www.w3.org/2001/XMLSchema');
-		$xml->appendChild($orderEl);
-
-		$orderDataEl = $xml->createElement('order_data');
+		$d->appendChild($orderEl);
+		$orderDataEl = $d->createElement('order_data');
 		$orderEl->appendChild($orderDataEl);
-		$this->addXmlChild($xml, $orderDataEl, 'order_date', $order->getCreatedAt());
-		$this->addXmlChild($xml, $orderDataEl, 'item_count', count($order->getAllVisibleItems()));
-		$this->addXmlChild($xml, $orderDataEl, 'total_item_amount', $order->getBaseSubtotal());
-		$this->addXmlChild($xml, $orderDataEl, 'channel', $this->storeManager->getStore($order->getStoreId())->getName());
-		$this->addXmlChild($xml, $orderDataEl, 'payment_method', $order->getPayment()->getMethod());
-		$this->addXmlChild($xml, $orderDataEl, 'seller_shipping_cost', '0');
-		$this->addXmlChild($xml, $orderDataEl, 'reference', $order->getIncrementId());
-		$this->addXmlChild($xml, $orderDataEl, 'client_id', $order->getCustomerId() ?: '');
-
-		$billing = $order->getBillingAddress();
-		$billingAddressEl = $xml->createElement('invoice_address');
+		$this->addXmlChild($d, $orderDataEl, 'order_date', $o->getCreatedAt());
+		$this->addXmlChild($d, $orderDataEl, 'item_count', count($o->getAllVisibleItems()));
+		$this->addXmlChild($d, $orderDataEl, 'total_item_amount', $o->getBaseSubtotal());
+		$this->addXmlChild($d, $orderDataEl, 'channel', $this->storeManager->getStore($o->getStoreId())->getName());
+		$this->addXmlChild($d, $orderDataEl, 'payment_method', $o->getPayment()->getMethod());
+		$this->addXmlChild($d, $orderDataEl, 'seller_shipping_cost', '0');
+		$this->addXmlChild($d, $orderDataEl, 'reference', $o->getIncrementId());
+		$this->addXmlChild($d, $orderDataEl, 'client_id', $o->getCustomerId() ?: '');
+		$billing = $o->getBillingAddress();
+		$billingAddressEl = $d->createElement('invoice_address');
 		$orderEl->appendChild($billingAddressEl);
-		$this->addXmlChild($xml, $billingAddressEl, 'address_id', $billing->getId());
-		$this->addXmlCDataChild($xml, $billingAddressEl, 'firstname', $billing->getFirstname());
-		$this->addXmlCDataChild($xml, $billingAddressEl, 'lastname', $billing->getLastname());
-		$this->addXmlChild($xml, $billingAddressEl, 'neighbourhood', '');
-		$this->addXmlCDataChild($xml, $billingAddressEl, 'street', $billing->getStreetLine(1));
-		$this->addXmlCDataChild($xml, $billingAddressEl, 'street_no', $billing->getStreetLine(2));
-		$this->addXmlChild($xml, $billingAddressEl, 'zip', $billing->getPostcode());
-		$this->addXmlCDataChild($xml, $billingAddressEl, 'city', $billing->getCity());
-		$this->addXmlChild($xml, $billingAddressEl, 'country', $billing->getCountryId());
-		$this->addXmlChild($xml, $billingAddressEl, 'email', $order->getCustomerEmail());
-		$this->addXmlChild($xml, $billingAddressEl, 'phone', $billing->getTelephone());
-		$this->addXmlChild($xml, $billingAddressEl, 'rfc', '');
-
-		$shipping = $order->getShippingAddress();
-		$shippingAddressEl = $xml->createElement('shipping_address');
+		$this->addXmlChild($d, $billingAddressEl, 'address_id', $billing->getId());
+		$this->addXmlCDataChild($d, $billingAddressEl, 'firstname', $billing->getFirstname());
+		$this->addXmlCDataChild($d, $billingAddressEl, 'lastname', $billing->getLastname());
+		$this->addXmlChild($d, $billingAddressEl, 'neighbourhood', '');
+		$this->addXmlCDataChild($d, $billingAddressEl, 'street', $billing->getStreetLine(1));
+		$this->addXmlCDataChild($d, $billingAddressEl, 'street_no', $billing->getStreetLine(2));
+		$this->addXmlChild($d, $billingAddressEl, 'zip', $billing->getPostcode());
+		$this->addXmlCDataChild($d, $billingAddressEl, 'city', $billing->getCity());
+		$this->addXmlChild($d, $billingAddressEl, 'country', $billing->getCountryId());
+		$this->addXmlChild($d, $billingAddressEl, 'email', $o->getCustomerEmail());
+		$this->addXmlChild($d, $billingAddressEl, 'phone', $billing->getTelephone());
+		$this->addXmlChild($d, $billingAddressEl, 'rfc', '');
+		$shipping = $o->getShippingAddress();
+		$shippingAddressEl = $d->createElement('shipping_address');
 		$orderEl->appendChild($shippingAddressEl);
-		$this->addXmlChild($xml, $shippingAddressEl, 'address_id', $shipping->getId());
-		$this->addXmlCDataChild($xml, $shippingAddressEl, 'firstname', $shipping->getFirstname());
-		$this->addXmlCDataChild($xml, $shippingAddressEl, 'lastname', $shipping->getLastname());
-		$this->addXmlChild($xml, $shippingAddressEl, 'neighbourhood', '');
-		$this->addXmlCDataChild($xml, $shippingAddressEl, 'street', $shipping->getStreetLine(1));
-		$this->addXmlCDataChild($xml, $shippingAddressEl, 'street_no', $shipping->getStreetLine(2));
-		$this->addXmlChild($xml, $shippingAddressEl, 'zip', $shipping->getPostcode());
-		$this->addXmlCDataChild($xml, $shippingAddressEl, 'city', $shipping->getCity());
-		$this->addXmlChild($xml, $shippingAddressEl, 'country', $shipping->getCountryId());
-		$this->addXmlChild($xml, $shippingAddressEl, 'phone', $shipping->getTelephone());
-
-		$itemsEl = $xml->createElement('items');
-		$xml->appendChild($itemsEl);
-
-		foreach ($order->getAllVisibleItems() as $item) {
-			/** @var Item $item */
-			$itemEl = $xml->createElement('item');
+		$this->addXmlChild($d, $shippingAddressEl, 'address_id', $shipping->getId());
+		$this->addXmlCDataChild($d, $shippingAddressEl, 'firstname', $shipping->getFirstname());
+		$this->addXmlCDataChild($d, $shippingAddressEl, 'lastname', $shipping->getLastname());
+		$this->addXmlChild($d, $shippingAddressEl, 'neighbourhood', '');
+		$this->addXmlCDataChild($d, $shippingAddressEl, 'street', $shipping->getStreetLine(1));
+		$this->addXmlCDataChild($d, $shippingAddressEl, 'street_no', $shipping->getStreetLine(2));
+		$this->addXmlChild($d, $shippingAddressEl, 'zip', $shipping->getPostcode());
+		$this->addXmlCDataChild($d, $shippingAddressEl, 'city', $shipping->getCity());
+		$this->addXmlChild($d, $shippingAddressEl, 'country', $shipping->getCountryId());
+		$this->addXmlChild($d, $shippingAddressEl, 'phone', $shipping->getTelephone());
+		$itemsEl = $d->createElement('items');
+		$d->appendChild($itemsEl);
+		foreach ($o->getAllVisibleItems() as $i) {/** @var OI $i */
+			$itemEl = $d->createElement('item');
 			$itemsEl->appendChild($itemEl);
-			$this->addXmlChild($xml, $itemEl, 'item_id', $item->getId());
-			$this->addXmlChild($xml, $itemEl, 'quantity', $item->getQtyOrdered());
-			$this->addXmlCDataChild($xml, $itemEl, 'label', $item->getName());
-			$this->addXmlChild($xml, $itemEl, 'item_price', $item->getBasePrice());
-			$this->addXmlChild($xml, $itemEl, 'carrier', '');
-			$this->addXmlChild($xml, $itemEl, 'tracking_code', '');
-			$this->addXmlChild($xml, $itemEl, 'status', $order->getStatus());
+			$this->addXmlChild($d, $itemEl, 'item_id', $i->getId());
+			$this->addXmlChild($d, $itemEl, 'quantity', $i->getQtyOrdered());
+			$this->addXmlCDataChild($d, $itemEl, 'label', $i->getName());
+			$this->addXmlChild($d, $itemEl, 'item_price', $i->getBasePrice());
+			$this->addXmlChild($d, $itemEl, 'carrier', '');
+			$this->addXmlChild($d, $itemEl, 'tracking_code', '');
+			$this->addXmlChild($d, $itemEl, 'status', $o->getStatus());
 		}
-
-		return $xml;
+		return $d;
 	}
 
 	/**
